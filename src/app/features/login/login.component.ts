d631a39776754ddf2bd36bb8f3eb38ec52f151b6
Main Kottaboung <64200002@kmitl.ac.th>
2025-05-23 12:20:34 +0700

    login system

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
 
-  constructor( private router: Router) {}
+  showModal = false;
+  modalTitle = '';
+  modalMessage = '';
+  isSuccess = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.valid) {
+      const { email, password } = this.form.value;
+      this.authService.login({ email, password }).subscribe({
+        next: () => {
+          this.modalTitle = 'Login success!';
+          this.isSuccess = true;
+          this.showModal = true;
+        },
+        error: (err) => {
+          this.modalTitle = 'Login failed '
+          this.modalMessage = err.error.message;
+          this.isSuccess = false;
+          this.showModal = true;
+        },
+      });
+    }
+  }
+
+  onModalClose() {
+    this.showModal = false;
+    if (this.isSuccess) {
+      this.router.navigate(['/home']); // หรือหน้า dashboard ที่ต้องการ
+    }
+  }
 
   goRegister() {
     this.router.navigate(['/register']);
